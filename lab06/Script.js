9335a9a1ac083d54b9f10d7409979efb0a00987c
Justyna Krotoszynska <justyna.krotoszynska@gmail.com>
2021-05-19 21:26:01 +0200

    update

--- lab06/Script.js
+++ lab06/Script.js
@@ -1,12 +1,27 @@
 window.setInterval(function () {
     const date = new Date();
-        const hours = document.querySelector('.hours');
-        const minutes = document.querySelector('.minutes');
-        const seconds = document.querySelector('.seconds');
+    const hours = document.querySelector('.hours');
+    const minutes = document.querySelector('.minutes');
+    const seconds = document.querySelector('.seconds');
 
-        hours.style.transform = `rotate(${date.getHours() / 12 * 360}deg)`;
-        minutes.style.transform = `rotate(${date.getMinutes() / 60 * 360}deg)`;
-        seconds.style.transform = `rotate(${date.getSeconds() / 60 * 360}deg)`;
+    hours.style.transform = `rotate(${date.getHours() / 12 * 360}deg)`;
+    minutes.style.transform = `rotate(${date.getMinutes() / 60 * 360}deg)`;
+    seconds.style.transform = `rotate(${date.getSeconds() / 60 * 360}deg)`;
+
+    const alarmH = date.getHours();
+    const alarmM = date.getMinutes();
+    const alarmS = date.getSeconds();
+
+    const sound = new Audio('sound.mp3');
+
+    if(alarmH == 21 && alarmM == 25 && alarmS == 35){
+        sound.play();
+    }
+
+    if(alarmH == 21 && alarmM == 25 && alarmS == 38){
+        alert("ALARM!!!!"); 
+    }
+
 
     for(let x = 1; x < 5; x++){
         const date = new Date;
